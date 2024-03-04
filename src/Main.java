c78ef8f32d0ee458cc976a7097e42df83e6f6287
BT_4N2_23 <timmy@gmail.com>
2024-03-04 13:57:15 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.io.FileWriter;
+
+public class Main {
+    public static void main(String[] args) {
+        String[][] maze = getMaze("data/maze1");
+
+    }
+    public static String[][] getMaze(String fileName) {
+        File f = new File(fileName);
+        Scanner s = null;
+        try {
+            s = new Scanner(f);
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("File not found.");
+            System.exit(1);
+        }
+
+        ArrayList<String> fileData = new ArrayList<String>();
+        while (s.hasNextLine())
+            fileData.add(s.nextLine());
+
+        int rows = fileData.size();
+        int cols = fileData.get(0).length();
+
+        String[][] maze = new String[rows][cols];
+
+        for (int i = 0; i < fileData.size(); i++) {
+            String d = fileData.get(i);
+            for (int j = 0; j < d.length(); j++) {
+                maze[i][j] = d.charAt(j) + "";
+            }
+        }
+        return maze;
+
+    }
